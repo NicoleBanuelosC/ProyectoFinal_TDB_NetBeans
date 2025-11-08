e796ecc3d96d1e65ad29040dac124922737e0e80
Nicole Bañuelos <nicole.bc@tecjerez.edu.mx>
2025-11-08 15:27:12 -0600

    Creación del repositorio

---
+++ src/main/java/com/mycompany/proyectofinal_tbd/ProyectoFinal_TBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal_tbd;
+
+/**
+ *
+ * @author banue
+ */
+public class ProyectoFinal_TBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
